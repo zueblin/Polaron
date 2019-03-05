1e93426fcae8b94667a2bcac8f18ffefc1790bd2
Thomas Zueblin <tom@innercity.ch>
2019-03-05 07:54:32 +0100

    added a delay to simpleDrumChannel

--- Software/Polaron/SimpleDrumChannel.h
+++ Software/Polaron/SimpleDrumChannel.h
@@ -21,31 +21,47 @@
 // SOFTWARE.
 #include <Audio.h>
 #include "AudioChannel.h"
+#include "effect_simple_delay.h"
 
 #ifndef SimpleDrumChannel_h
 #define SimpleDrumChannel_h
 
 class SimpleDrumChannel : public AudioChannel {
    public:
-    SimpleDrumChannel(int lowFreq, int highFreq) {
+    SimpleDrumChannel(int lowFreq, int highFreq)
+        : drumToMix(drum, 0, mixer, 0), mixToDelay(mixer, 0, deelay, 0), delayToAmp(deelay, 0, amp, 0), ampToMix(amp, 0, mixer, 1) {
         low = lowFreq;
         high = highFreq;
+        mixer.gain(0, 1.0f);
+        mixer.gain(1, 0.9f);
+        deelay.delay(0, 128.0f);
+        //deelay.delay(1, 256.0f);
+        // deelay.delay(1, 513.0f);
     }
-    AudioStream *getOutput1() { return &drum; }
-    AudioStream *getOutput2() { return &drum; }
+    AudioStream *getOutput1() { return &mixer; }
+    AudioStream *getOutput2() { return &mixer; }
 
     void trigger() { drum.noteOn(); }
     void setParam1(int value) { drum.frequency(32.0f + value * 10.0f); }
     void setParam2(int value) { drum.pitchMod(value / 1024.0f); }
     void setParam3(int value) { drum.secondMix(value / 1024.0f); }
     void setParam4(int value) { drum.length(value); }
-    void setParam5(int value) {}
-    void setParam6(int value) {}
+    void setParam5(int value) { deelay.delay(0, value / 8.0f); }
+    void setParam6(int value) { amp.gain(value / -1024.0f);}
 
    private:
     int low = 35;
     int high = 880;
+    int f1 = 100;
+    int f2 = 0;
     AudioSynthSimpleDrum drum;
+    AudioMixer4 mixer;
+    AudioEffectSimpleDelay deelay;
+    AudioAmplifier amp;
+    AudioConnection drumToMix;
+    AudioConnection mixToDelay;
+    AudioConnection delayToAmp;
+    AudioConnection ampToMix;
 };
 
 #endif
